ab9ee0316f15c0cd951671843be1feabd9c442a0
kegansovay <kegan.sovay@gmail.com>
2024-03-08 08:59:47 -0800

    new logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const Logo = () => (
-  <svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect width="100" height="100" fill="black"/>
-<path d="M60.5576 36.5148V86H39.4424V36.5148H23V16H77V36.5148H60.5576Z" fill="white"/>
+  <svg width="78" height="56" viewBox="0 0 78 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect width="13.4628" height="13.4628" transform="matrix(0.724167 0.689624 -0.724167 0.689624 48.7529 0)" fill="currentColor"/>
+<rect width="13.4628" height="13.4628" transform="matrix(0.724167 0.689624 -0.724167 0.689624 68.248 18.5723)" fill="currentColor"/>
+<rect width="13.4628" height="13.4628" transform="matrix(0.724167 0.689624 -0.724167 0.689624 29.25 18.5723)" fill="currentColor"/>
+<rect width="13.4628" height="13.4628" transform="matrix(0.724167 0.689624 -0.724167 0.689624 48.7529 37.1436)" fill="currentColor"/>
+<rect width="13.4628" height="13.4628" transform="matrix(0.724167 0.689624 -0.724167 0.689624 29.25 0)" fill="currentColor"/>
+<rect width="13.4628" height="13.4628" transform="matrix(0.724167 0.689624 -0.724167 0.689624 48.7529 18.5723)" fill="currentColor"/>
+<rect width="13.4628" height="13.4628" transform="matrix(0.724167 0.689624 -0.724167 0.689624 9.74902 18.5723)" fill="currentColor"/>
+<rect width="13.4628" height="13.4628" transform="matrix(0.724167 0.689624 -0.724167 0.689624 29.25 37.1436)" fill="currentColor"/>
 </svg>
 
 )
