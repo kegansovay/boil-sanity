2c5f8e23f071b714c5eec52d56a6fd37a37ccf72
kegansovay <kegan.sovay@gmail.com>
2024-03-20 08:57:40 -0700

    added some definefields

--- schemaTypes/documents/page.ts
+++ schemaTypes/documents/page.ts
@@ -1,35 +1,35 @@
 import {defineField} from 'sanity'
 import PrefixedSlugInput from '../../components/inputs/SlugInput'
 
-
 export default defineField({
   name: 'page',
   title: 'Page',
   type: 'document',
   fields: [
-    {
+    defineField({
       name: 'name',
       title: 'Name',
       type: 'string',
-    },
-    {
+    }),
+    defineField({
       name: 'slug',
       title: 'Slug',
       type: 'slug',
       components: {input: PrefixedSlugInput},
       options: {
         source: 'name',
+        //@ts-ignore
         urlPrefix: 'about',
         maxLength: 200,
         storeFullUrl: true,
       },
       validation: (Rule) => Rule.required(),
-    },
-    {
+    }),
+    defineField({
       name: 'seo',
       title: 'SEO',
       type: 'seo',
       validation: (Rule) => Rule.required(),
-    },
+    }),
   ],
 })
