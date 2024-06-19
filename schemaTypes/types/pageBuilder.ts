40c2090c6b2fea01822ee39ce67ff0de9478fba2
kegansovay <kegan.sovay@gmail.com>
2024-06-19 09:19:51 -0400

    switched to sanity module picker...

--- schemaTypes/types/pageBuilder.ts
+++ schemaTypes/types/pageBuilder.ts
@@ -1,12 +1,21 @@
-import { defineField } from "sanity";
-import ModulePickerInput from "../../components/inputs/ModulePickerInput";
+import {defineField} from 'sanity'
 
 export default defineField({
-    name: 'pageBuilder',
-    title: 'Page Builder',
-    type: 'array',
-    components: {input: ModulePickerInput},
-    of: [
-        {type: 'link'}
-    ]
+  name: 'pageBuilder',
+  title: 'Page Builder',
+  type: 'array',
+  options: {
+    insertMenu: {
+      filter: 'auto',
+      showIcons: true,
+      views: [
+        {
+          name: 'grid',
+          previewImageUrl: (schemaTypeName) => `/static/modules/${schemaTypeName}.jpg`,
+        },
+      ],
+      groups: [{name: 'hero', of: ['hero']}],
+    },
+  },
+  of: [{type: 'link'}],
 })
